0a674638d8010ee4eff969c17fc602fc30c1ce04
Gamonly <143465484+Gamonly@users.noreply.github.com>
2024-06-29 01:14:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+const khodams = [
+    { name: 'Curut Kejepit'},
+    { name: 'Sule Betawi'},
+    { name: 'Lumba-Lumba Jawa'},
+    { name: 'Laba-Laba Sunda'},
+    { name: 'Nyai Roro Kidul Montok'},
+    { name: 'Musang King'},
+    { name: 'Kuntilanak Lanang'},
+    { name: 'Rawa Rontek'},
+    { name: 'Suster Ngepod'},
+    { name: 'Gondrong Baper'},
+    { name: 'Reza Kecap'},
+    { name: 'Icikiwier'},
+    { name: 'Farhan Kebab'},
+    { name: 'Monyet Second Choice'},
+    { name: 'Lidi Terbang'},
+    { name: 'Telo Rebus'},
+    { name: 'Pocong Ngopi'},
+    { name: 'Bantal Busa'},
+    { name: 'Tikus Berdasi'},
+    { name: 'Bolu Kukus'},
+    { name: 'Pindang Kering'},
+    { name: 'Badut Lonely'},
+    { name: 'Terasi Wangi'},
+    { name: 'Botol Yakult'},
+    { name: 'Kura-Kura Penghibur'},
+    { name: 'Buaya Setia'},
+    { name: 'Ubi Singset'},
+    { name: 'Anjing Lucu'},
+    { name: 'Bebek Ngigau'},
+    { name: 'Tuyul Ngompol'},
+    { name: 'Wajan Lepek'},
+
+
+    // Tambahkan lebih banyak khodam sesuai kebutuhan
+];
+
+window.onload = () => {
+    const backgroundMusic = document.getElementById('backgroundMusic');
+    backgroundMusic.play();
+};
+
+function generateKhodam() {
+    const name = document.getElementById('nameInput').value.trim();
+    if (name === "") {
+        alert("God said: Masukkan nama Anda terlebih dahulu!");
+        return;
+    }
+    const randomIndex = Math.floor(Math.random() * khodams.length);
+    const selectedKhodam = khodams[randomIndex];
+    
+    document.getElementById('userName').innerText = name;
+    document.getElementById('khodamName').innerText = selectedKhodam.name;
+
+    document.getElementById('mainContainer').style.display = 'none';
+    document.getElementById('resultContainer').style.display = 'block';
+}
+
+function reset() {
+    document.getElementById('nameInput').value = '';
+    document.getElementById('mainContainer').style.display = 'block';
+    document.getElementById('resultContainer').style.display = 'none';
+}
+
+// Tooltip untuk watermark
+tippy('.watermark', {
+    content: "Klik untuk membuka profil",
+    placement: 'top',
+    arrow: true,
+});
